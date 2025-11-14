e53838936c41959a015d0be3eb6e81510f95a441
Nataliya <mom.natik@gmail.com>
2025-11-14 16:17:38 +0300

    + readme features list

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # tickettoride
 Ticket To Ride documents, tools
+
+# User features
+## Total
+## Implemented
+- main page
+    - game options
+        - number of players
+        - cities count
+        - play with computer
+    - start game
+- game page
+    - default map background
+    - map background landscape
+
+# Architecture
+- WEB
+- API (Gateway)
+- Game API
+- Game Service
+- Map Service
+- Lexis API
--- scripts-app/run-dev.cmd
+++ scripts-app/run-dev.cmd
@@ -1,6 +1,6 @@
-@REM start /d tickettoride- npm run dev
-@REM start /d tickettoride- npm run dev
-start /d "tickettoride-game-service/src" npm run dev
-start /d "tickettoride-game-api/src" npm run dev
-start /d "tickettoride-api/src" npm run dev
-start /d "tickettoride-web/src" npm run dev
+@REM start /d tickettoride- npm run exec
+@REM start /d tickettoride- npm run exec
+start /d "../../tickettoride-game-service/project/src" npm run exec
+start /d "../../tickettoride-game-api/project/src" npm run exec
+start /d "../../tickettoride-api/project/src" npm run exec
+start /d "../../tickettoride-web/project/src" npm run exec
